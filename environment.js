52e42dc7d8e7a547a5dac83a7bd236e4c410b2e8
siron azevedo <sirondba@gmail.com>
2021-03-04 20:24:24 -0300

    Ajuste file environment

--- environment.js
+++ environment.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 
-if(${process.env.ENVIRONMENT} !== null || ${process.env.ENVIRONMENT} !== undefined) {
-    const heroku = `export const environment = {
-        production: true,
-        ambiente: '${process.env.ENVIRONMENT}'
-    }`
-    
-    fs.writeFile('src/environments/environment.prod.ts', heroku, (err, result) => {
-        if(err){
-            console.log('Falha ao escrever arquivo com variavel de ambiente');
-        }
-    });
-}
+const value = process.env.ENVIRONMENT || 'local'
+const heroku = `export const environment = {
+    production: true,
+    ambiente: '${value}'
+}`
+
+fs.writeFile('src/environments/environment.prod.ts', heroku, (err, result) => {
+    if(err){
+        console.log('Falha ao escrever arquivo com variavel de ambiente');
+    }
+});
