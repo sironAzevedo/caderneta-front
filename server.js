f480c968d48923578748f51d4ec0a17bcde8aed1
siron azevedo <sirondba@gmail.com>
2021-03-05 00:22:53 -0300

    Ajuste file environment

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express();
  
-window.localStorage.setItem('env', process.env.ENVIRONMENT || 'local');
 app.use(express.static(__dirname + '/dist/caderneta-front'));
 
 app.get('/*', (req, res) => {
